ecf5556e7fd8055ee22cb0701eb9e0b6433feae9
Hanksss <31931168+Hanksss@users.noreply.github.com>
2020-02-22 16:55:05 -0700

    fixed problem

---
+++ index.js
@@ -0,0 +1,43 @@
+var ourMessage = $("#theH");
+if('webkitSpeechRecognition' in window)
+{
+	var final_transcript = "";
+	var interim_transcript = "";
+	ourMessage.text("Yes");
+	console.log("speechAvaibalbe?: Yes");
+	var recognition = new webkitSpeechRecognition();
+	  recognition.continuous = true;
+	  recognition.interimResults = false;
+	  recognition.lang = "en-US";
+	  recognition.start();
+	 
+	 recognition.onstart = function()
+	 {
+		console.log("Yes, it started");
+		
+		recognition.onresult = function(e) {
+		console.log("You should have a result right now...");
+    var interim_transcript = '';
+
+    for (var i = e.resultIndex; i < e.results.length; ++i) {
+      if (e.results[i].isFinal) {
+        final_transcript += e.results[i][0].transcript;
+      } else {
+        interim_transcript += e.results[i][0].transcript;
+      }
+    }
+    
+	console.log(final_transcript);
+    $("#theInput").val(final_transcript);
+    $("#theOtherInput").val(interim_transcript);
+  };
+  
+  recognition.onerror = function(event) {
+    console.log(event.error);
+	};
+	 };
+}
+else
+{
+	ourMessage.text("No");
+}
