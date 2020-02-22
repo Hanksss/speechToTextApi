1b8b9399331833ef3511af7dc103665c13b2695a
Alioune Yague <ayague@asu.edu>
2020-02-22 14:53:05 -0700

    New changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var ourMessage = $("#theH");
 if('webkitSpeechRecognition' in window)
 {
+	var final_transcript = "";
+	var interim_transcript = "";
 	ourMessage.text("Yes");
 	console.log("speechAvaibalbe?: Yes");
 	var recognition = new webkitSpeechRecognition();
@@ -9,8 +11,12 @@ if('webkitSpeechRecognition' in window)
 	  recognition.lang = "en-US";
 	  recognition.start();
 	 
-	
-	recognition.onresult = function(e) {
+	 recognition.onstart = function()
+	 {
+		console.log("Yes, it started");
+		
+		recognition.onresult = function(e) {
+		console.log("You should have a result right now...");
     var interim_transcript = '';
 
     for (var i = e.resultIndex; i < e.results.length; ++i) {
@@ -21,9 +27,15 @@ if('webkitSpeechRecognition' in window)
       }
     }
     final_transcript = capitalize(final_transcript);
+	console.log(final_transcript);
     $("#theInput").val(linebreak(final_transcript));
     $("#theOtherInput").val(linebreak(interim_transcript));
   };
+  
+  recognition.onerror = function(event) {
+    console.log(event.error);
+	};
+	 };
 }
 else
 {
